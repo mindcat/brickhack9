6f9c2884ac8068b0d2878cba42bcfc75ae16edcb
aln4739 <henryy20022@gmail.com>
2023-02-25 19:33:06 -0500

    setup post /stars

--- API/src/Stars.jl
+++ API/src/Stars.jl
@@ -12,9 +12,10 @@ struct Star
     spectrum :: Char
 end
 
-function read_data(filename)
+# Only need to be run once to format
+function _read_data(filename)
     stars = CSV.read(filename, DataFrames.DataFrame)
-    ret = []
+    to_write = []
     num = 1
     for row in eachrow(stars)
         # handle missing proper names
@@ -29,7 +30,7 @@ function read_data(filename)
         if row.Mag > 5.5
             continue
         end
-        push!(ret, Star(num, 
+        push!(to_write, Star(num, 
         row.ProperName, 
         row.RA, 
         row.Dec, 
@@ -39,7 +40,67 @@ function read_data(filename)
         row.Spectrum[1]))
         num += 1
     end
+    
+    CSV.write("$(@__DIR__)/data/stars_trimmed.csv", to_write)
+end
+
+# Get star color from spectrum
+function star_color(star)
+    if star.spectrum == 'K'
+        color = 14
+    elseif star.spectrum == 'G'
+        color = 15
+    elseif star.spectrum == 'F'
+        color = 16
+    elseif star.spectrum == 'A'
+        color = 17
+    elseif star.spectrum == 'B'
+        color = 18
+    elseif star.spectrum == 'O'
+        color = 19
+    else # otherwise, just make the star an M type spectrum
+        color = 13
+    end
+    
+    return color
+end
+
+# Get the stars from the trimmed file
+function get_stars()
+    data = CSV.read("$(@__DIR__)/data/stars_trimmed.csv", DataFrames.DataFrame)
+    ret = []
+    for r in eachrow(data)
+        if isequal(r.name, missing)
+            r.name = ""
+        end
+        push!(ret, Star(
+            r.id, 
+            r.name, 
+            r.ra, 
+            r.dec, 
+            r.distance, 
+            r.mag, 
+            r.absmag, 
+            r.spectrum[1]
+            ))
+    end
     return ret
 end
+
+# Return the star coordinates to visualize
+function get_star_coordinates(stars)
+    star_coordinates = []
+    for star in stars
+        item = Dict(
+            :ra => star.ra,
+            :dec => star.dec,
+            :mag => star.mag,
+            :color => star_color(star)
+        )
+        push!(star_coordinates, item)
+    end
+    return star_coordinates
+end
+
 
 end # end module
--- API/src/StellarAPI.jl
+++ API/src/StellarAPI.jl
@@ -6,38 +6,14 @@ using StructTypes
 
 include("Stars.jl")
 
+# Only need this if returning any Star structs
 StructTypes.StructType(::Type{Stars.Star}) = StructTypes.Struct()
 
 # All the stars in a vector
-stars = Stars.read_data("$(@__DIR__)/data/stars.csv")
-
-function star_type(star)
-    type = 12
-    if star.spectrum == 'M'
-        type = 13
-    elseif star.spectrum == 'K'
-        type = 14
-    elseif star.spectrum == 'G'
-        type = 15
-    elseif star.spectrum == 'F'
-        type = 16
-    elseif star.spectrum == 'A'
-        type = 17
-    elseif star.spectrum == 'B'
-        type = 18
-    elseif star.spectrum == 'O'
-        type = 19
-    else 
-        type = 13
-end
-
-tup_stars = []
-for star in stars 
-    push!(tup_stars, (star.ra, star.dec, star.mag, star_type(star)))
-end
+stars = Stars.get_stars()
 
 @post "/stars" function(req::HTTP.Request)
-    return stars
+    return Stars.get_star_coordinates(stars)
 end
 
 # start the web server
