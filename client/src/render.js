a9dd3b119af4c9b757520b7718b61293d34d5841
mindcat <maw4678@rit.edu>
2023-02-26 03:22:05 -0500

    not working but i need to sleep

--- client/src/render.js
+++ client/src/render.js
@@ -1,5 +1,5 @@
-
-let canvas = document.getElementById('canvas');
+// let canvas = document.getElementById('canvas');
+// var csv = require('jquery-csv');
 // canvas.width = window.innerWidth * 0.8;
 // canvas.height = window.innerHeight * 0.8;
 
@@ -11,6 +11,18 @@ let canvas = document.getElementById('canvas');
 let TAU = Zdog.TAU;
   
 // isSpinning = true;
+
+function coordinates(ra, dec) {
+    var cosRA = Math.cos(ra * Math.PI / 180.0);
+    var sinRA = Math.sin(ra * Math.PI / 180.0);
+    var cosDec = Math.cos(dec * Math.PI / 180.0);
+    var sinDec = Math.sin(dec * Math.PI / 180.0);
+    var rad = 500.0;
+    x = rad * cosRA * cosDec;
+    y = rad * cosRA * sinDec;
+    z = rad * sinRA;
+    return [x, y, z];
+}
 
 let illo = new Zdog.Illustration({
   element: ".zdog-canvas",
@@ -30,6 +42,9 @@ let illo = new Zdog.Illustration({
     this.zoom = zoom < 10 && 10 || zoom;
   }
 });
+
+let stars = [[0.02662528,-77.06529438],[-3.02747891,125.31328320802],[18.40287397,-34.3843146],[14.66094188,-60.83947139]]
+// [[1,,0.02662528,-77.06529438,67.7048070412999,4.78,0.62690246,K],[2,,0.03039927,-3.02747891,125.31328320802,5.13,-0.35998553,B],[3,,0.03266433,-6.01397169,127.226463104326,4.37,-1.1528872,M],[4,,0.03886504,-29.72044805,156.25,5.04,-0.92910016,B],[5,,0.06232565,-17.33597002,69.8812019566736,4.55,0.32819816,B],[6,,0.07503398,-10.50949443,492.610837438424,4.99,-3.4725199,K]]
 
 planets = {
     sun: {
@@ -206,14 +221,23 @@ planets = {
 //     element: '.zdog-canvas',
 //     dragRotate: true,
 // });
+for (star in stars) {
+    var [x, y, z] = coordinates(star[0], star[1]);
+    // if (x < 125 || y < 125 || z < 125 ) {
+    //     x = x*25;
+    //     z = z*25;
+    //     y = y*25;
+    // }
+    // add star
+    new Zdog.Shape({
+        addTo: illo,
+        translate: {x: x, y: y, z: z},
+        diameter: 10,
+        stroke: 2,
+        color: '#ffffff',
+    });
 
-// add circle
-new Zdog.Ellipse({
-    addTo: illo,
-    diameter: 8,
-    stroke: 2,
-    color: '#636',
-});
+}
 
 n = 0;
 
@@ -222,7 +246,7 @@ function animate() {
         planet = planets[k];
         planet.orbitAnchor.rotate.y -= TAU / planet.orbitPeriod / 5;
         if (planet.satelliteOf && planet.satelliteOf !== "sun") {
-          planet.anchor.rotate.y += TAU / planets[planet.satelliteOf].orbitPeriod / 5;
+          planet.anchor.rotate.y += TAU / planets[planet.satelliteOf].orbitPeriod / 365;
         }
     }
     // rotate illo each frame
